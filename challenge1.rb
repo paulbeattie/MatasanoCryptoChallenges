de064d0a9e88e486d90948bc9c380da18b3007fc
Paul Beattie <paul@paul-beattie.com>
2015-03-19 20:23:34 +0000

    Completing challenge 1

---
+++ challenge1.rb
@@ -0,0 +1,9 @@
+require 'base64'
+
+str = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+EXPECTED_STR = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
+
+hex_string = [str].pack('H*')
+base_string =  Base64.strict_encode64(hex_string)
+p base_string
+p base_string == EXPECTED_STR
